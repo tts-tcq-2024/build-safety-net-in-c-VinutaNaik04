991f00b2516e22798d5885b400c11488c755b14f
VinutaNaik04 <vinutanaik456@gmail.com>
2024-08-07 21:18:23 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -7,7 +7,20 @@
 
 char getSoundexCode(char c) {
     c = toupper(c);
-    switch (c) {
+    if(c=='B'||c=='F'||c=='P'||c=='V'||c=='b'||c=='f'||c=='p'||c=='v')
+        return '1';
+    if (c=='C'||c=='c'||c=='G'||c=='g'||c=='J'||c=='j'||c=='K'||c=='k'||c=='Q'||c=='q'||c=='S'||c=='s'||c=='X'||c=='x'||c=='Z'||c=='z')
+        return '2';
+    if(c=='D'||c=='T'||c=='d'||c=='t')
+        return '3';
+    if(c=='L'||c=='l')
+        return '4';
+    if(c=='M'||c=='m'||c=='N'||c=='n')
+        return '5';
+     if(c=='R'||c=='r')
+        return '6';
+    return '0'
+   /* switch (c) {
         case 'B': case 'F': case 'P': case 'V': return '1';
         case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
         case 'D': case 'T': return '3';
@@ -15,7 +28,7 @@ char getSoundexCode(char c) {
         case 'M': case 'N': return '5';
         case 'R': return '6';
         default: return '0'; // For A, E, I, O, U, H, W, Y
-    }
+    }*/
 }
 
 void generateSoundex(const char *name, char *soundex) {

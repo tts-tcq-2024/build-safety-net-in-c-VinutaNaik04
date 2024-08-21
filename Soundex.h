2412bceefe9234406bad866a9cd55af9f47c9746
VinutaNaik04 <vinutanaik456@gmail.com>
2024-08-21 12:59:35 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -4,15 +4,43 @@
 #include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
+ #include <ctype.h>
+
+char value_check()
 
 char getSoundexCode(char c) {
     c = toupper(c);
-    char return_c;
-   /* return_c=(c=='B'||c=='F'||c=='P'||c=='V'||c=='b'||c=='f'||c=='p'||c=='v')?'1':
-              (c=='C'||c=='c'||c=='G'||c=='g'||c=='J'||c=='j'||c=='K'||c=='k'||c=='Q'||c=='q'||c=='S'||c=='s'||c=='X'||c=='x'||c=='Z'||c=='z')?'2':
-               (c=='D'||c=='T'||c=='d'||c=='t')?'3':
-                (c=='L'||c=='l')?'4':(c=='M'||c=='m'||c=='N'||c=='n')?'5':(c=='R'||c=='r')?'6':'0';*/
-    return return_c;
+    switch (c)
+        {
+    case 'B:
+    case 'F':
+    case 'P':
+    case 'V':
+        return '1';
+    case 'C':
+    case 'G':
+    case 'J':
+    case 'K':
+        
+    case 'Q':
+    case 'S':
+    case 'X':
+    case 'Z':
+        return '2';
+               
+    case 'D':
+    case 'T':
+        return '3';
+    case 'L':
+        return '4';
+    case 'M':
+    case 'N':
+        return '5';
+    case 'R':
+        return '6';
+        default :
+            return '0';
+        }
 }
 
 void generateSoundex(const char *name, char *soundex) {

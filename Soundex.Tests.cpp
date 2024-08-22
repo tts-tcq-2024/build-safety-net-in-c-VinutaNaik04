72cb4977a493c5163833e6f4c271e8cc7d5ec116
VinutaNaik04 <vinutanaik456@gmail.com>
2024-08-22 17:09:26 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,15 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+/*TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("AXxxx", soundex);
  ASSERT_EQ(soundex,"A200");
+}*/
+TEST(SoudexTestsuite, BasicSoundexcode)
+{
+ char soundex[5];
+  generateSoundex("AXxxx", soundex);
+ ASSERT_EQ(std :: string(soundex),"A200");
 }

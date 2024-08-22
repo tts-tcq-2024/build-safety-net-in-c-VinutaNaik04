086b7b35591c49bfd615de02af961165dae05e51
VinutaNaik04 <vinutanaik456@gmail.com>
2024-08-22 20:42:29 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -2,8 +2,9 @@
 #include "Soundex.h"
 TEST(SoundexTestsuite, BasicSoundexCode) {
     char soundex[5];
-    generateSoundex("A0V0", soundex);
-    ASSERT_EQ(std::string(soundex), "A010");
+   generateSoundex("Example", soundex);
+EXPECT_STREQ(soundex, "E251");
+
 }
 
 TEST(SoundexTestsuite, IgnoresNonAlphabeticCharacters) {

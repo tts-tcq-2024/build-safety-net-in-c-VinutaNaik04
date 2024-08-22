4abc1b5ef5870905e7448ad5b10a404634b7815c
VinutaNaik04 <vinutanaik456@gmail.com>
2024-08-22 17:11:19 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,15 +1,39 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
-
-/*TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
-  generateSoundex("AXxxx", soundex);
- ASSERT_EQ(soundex,"A200");
-}*/
-TEST(SoudexTestsuite, BasicSoundexcode)
+  generateSoundex("AX", soundex);
+  EXPECT_STREQ(soundex,"A200");
+}
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits1)
 {
- char soundex[5];
-  generateSoundex("AXxxx", soundex);
- ASSERT_EQ(std :: string(soundex),"A200");
+  char soundex[5];
+  generateSoundex("X0", soundex);
+ EXPECT_STREQ(soundex,"X200");
+}
+
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits2)
+{
+  char soundex[5];
+  generateSoundex("ZQOP", soundex);
+ EXPECT_STREQ(soundex,"Z210");
+}
+
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits3)
+{
+  char soundex[5];
+  generateSoundex("ASDSD", soundex);
+ EXPECT_STREQ(soundex,"A232");
+}
+
+
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits4)
+{
+  char soundex[5];
+  generateSoundex("12345", soundex);
+ EXPECT_STREQ(soundex,"1000");
 }
